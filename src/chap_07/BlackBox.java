63ba2b8803261c69b6e85590827ddf3507327967
Sanghyun Lee <tkdguslee92@gmail.com>
2023-04-26 22:15:32 +0900

    클래스 변수 공부했음.

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -5,6 +5,7 @@ public class BlackBox {
     String resolution; // 해상도
     int price; // 가격
     String color; // 색상
-
     // 인스턴스변수 또는 필드라고도 함
+    static boolean canAutoReport = false; // 자동 신고 기능
+    // 여기서 static을 붙이면 클래스 변수라고함
 }

310330d519fc5d702ab3a6073ef58fa96099d266
Sanghyun Lee <tkdguslee92@gmail.com>
2023-04-26 21:48:32 +0900

    인스턴스 변수 공부했음

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -1,5 +1,10 @@
 package chap_07;
 
 public class BlackBox {
+    String modelName; // 모델명
+    String resolution; // 해상도
+    int price; // 가격
+    String color; // 색상
 
+    // 인스턴스변수 또는 필드라고도 함
 }

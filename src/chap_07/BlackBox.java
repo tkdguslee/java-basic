a81900a9b1a2ddcab22d1619e5dc14893ae211ca
Sanghyun Lee <tkdguslee92@gmail.com>
2023-04-27 22:31:05 +0900

    메소드 공부 했음.

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -8,4 +8,28 @@ public class BlackBox {
     // 인스턴스변수 또는 필드라고도 함
     static boolean canAutoReport = false; // 자동 신고 기능
     // 여기서 static을 붙이면 클래스 변수라고함
+
+    void autoReport() {
+        if (canAutoReport) {
+            System.out.println("충돌이 감지되어 자동으로 신고합니다.");
+        }
+        else {
+            System.out.println("자동 신고 기능이 지원되지 않습니다.");
+        }
+    }
+
+    void insertMemoryCard(int capacity) {
+        System.out.println("메모리 카드가 삽입되었습니다.");
+        System.out.println("용량은 " + capacity + "GB 입니다.");
+    }
+
+    int getVideoFileCount(int type) {
+        if (type == 1) { // 일반 영상
+            return 9;
+        }
+        else if (type == 2) { // 이벤트 영상
+            return 1;
+        }
+        return 10;
+    }
 }

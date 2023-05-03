aa6805ae4e71de4359986134efa15a46a67ff39f
Sanghyun Lee <tkdguslee92@gmail.com>
2023-05-03 22:16:33 +0900

    패키지 공부했음.

--- src/chap_08/_00_AccessModifierTest.java
+++ src/chap_08/_00_AccessModifierTest.java
@@ -1,6 +1,8 @@
 package chap_08;
 
-import chap_07.BlackBoxRefurbish;
+import chap_07.*;
+// import chap_07.BlackBoxRefurbish; // 07에 패키지에 있는 BlackBoxRefurbish를 쓰겠다라는 뜻이다!
+// import chap_07. * // 07 패키지에 있는거 다부른다
 
 public class _00_AccessModifierTest {
     public static void main(String[] args) {
@@ -9,5 +11,7 @@ public class _00_AccessModifierTest {
         // b1.resolution = "FHD"; // default
         // b1.price = 200000; // private
         // b1.color = "블랙"; // protected
+
+        BlackBox b2;
     }
 }

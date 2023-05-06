95cca77bac7e068ff9bfb34740f62327b6735574
Sanghyun Lee <tkdguslee92@gmail.com>
2023-05-06 22:20:38 +0900

    메소드 오버라이딩 공부했음.

--- src/chap_07/camera/Camera.java
+++ src/chap_07/camera/Camera.java
@@ -17,6 +17,9 @@ public class Camera { // 부모 클래스
     public void recordVideo() {
         // 동영상 녹화
         System.out.println(this.name + " : 동영상을 녹화합니다.");
+    }
 
+    public void showMainFeature() {
+        System.out.println(this.name + "의 주요 기능 : 사진 촬영, 동영상 녹화");
     }
 }
--- src/chap_07/camera/FactoryCam.java
+++ src/chap_07/camera/FactoryCam.java
@@ -13,4 +13,8 @@ public class FactoryCam extends Camera { // 자식 클래스
         // 화재 감지
         System.out.println("화재를 감지합니다.");
     }
+
+    public void showMainFeature() {
+        System.out.println(this.name + "의 주요 기능 : 화재 감지");
+    }
 }
